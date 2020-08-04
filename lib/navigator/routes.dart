c47d4323eb0c616eef85747550135a413baa3cd1
oscarnar <orosas58@gmail.com>
2020-08-04 15:48:00 -0500

    Funcion para el generador de rutas de la app

---
+++ lib/navigator/routes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:squirtle/interface/home/home.dart';
+import 'package:squirtle/interface/login/login.dart';
+import 'package:squirtle/interface/mobileScreen.dart';
+import 'package:squirtle/interface/profile/profile.dart';
+import 'package:squirtle/navigator/route_names.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case Routes.MobileRoute:
+      return MaterialPageRoute(builder: (context) => LayoutMobileScreen());
+    case Routes.LoginRoute:
+      return MaterialPageRoute(builder: (context) => LoginScreen());
+    case Routes.HomeRoute:
+      return MaterialPageRoute(builder: (context) => HomeScreen());
+    case Routes.TaskRoute:
+      return MaterialPageRoute(builder: (context) => VistaPrueba('Task Screen'));
+    case Routes.ProfileRoute:
+      return MaterialPageRoute(builder: (context) => ProfileScreen());
+    default:
+      return MaterialPageRoute(builder: (context) => LoginScreen());
+  }
+}
