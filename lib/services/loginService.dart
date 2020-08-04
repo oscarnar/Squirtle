83203ade59a5bead19e925a6e4b1d1092631fe44
oscarnar <orosas58@gmail.com>
2020-08-04 15:45:09 -0500

    Servicio login para obtener el token de la API

---
+++ lib/services/loginService.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:squirtle/services/server.dart';
+
+Future<dynamic> loginService(
+    {@required String username, @required String password}) async {
+  var url = Server.LoginURL;
+  var valueJSON;
+  var response = await http.post(url, body: {
+    'username': username,
+    'password': password,
+    'service': Server.LoginService,
+  });
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+  valueJSON = json.decode(response.body);
+  
+  return valueJSON;
+}
