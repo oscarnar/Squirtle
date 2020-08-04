8c2071ec700ebd28bda6982bd8c768988dbe8f11
oscarnar <orosas58@gmail.com>
2020-08-04 15:48:22 -0500

    Clase para mapear los nombres de las rutas

---
+++ lib/navigator/route_names.dart
@@ -0,0 +1,7 @@
+class  Routes  {
+  static const String LoginRoute = "/";
+  static const String MobileRoute = "/mobile";
+  static const String HomeRoute = "/home";
+  static const String TaskRoute = "/task";
+  static const String ProfileRoute = "/profile";
+}
