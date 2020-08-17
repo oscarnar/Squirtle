84812be948a5d41f0abb49105a9aa62b00120cd9
oscarnar <orosas58@gmail.com>
2020-08-17 00:59:50 -0500

    Fix some errors

--- lib/interface/mobileScreen.dart
+++ lib/interface/mobileScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:squirtle/interface/components/appBar.dart';
+import 'package:squirtle/interface/components/drawer.dart';
 import 'package:squirtle/models/userModel.dart';
 import 'package:squirtle/navigator/route_names.dart';
 import 'package:squirtle/navigator/routes.dart';
@@ -23,60 +24,50 @@ class _LayoutMobileScreenState extends State<LayoutMobileScreen> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+    final User user = Provider.of<UserProvider>(context).user;
     return Scaffold(
       appBar: buildAppBar(size),
-      drawer: Drawer(
-          child: ListView(
-        children: [
-          RaisedButton(
-            child: Text('Logout'),
-            onPressed: () {
-              Provider.of<UserProvider>(context,listen: false).user = User();
-              Provider.of<CourseProvider>(context,listen: false).listCourses = [];
-              Navigator.pushNamedAndRemoveUntil(
-                context,
-                Routes.LoginRoute,
-                (Route<dynamic> route) => false,
-              );
-            },
-          )
-        ],
-      )),
+      drawer: MyDrawer(),
       body: Navigator(
         key: Get.nestedKey(1),
         onGenerateRoute: generateRoute,
         initialRoute: listRoutes[_currentIndex],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.black,
-        type: BottomNavigationBarType.fixed,
-        showUnselectedLabels: true,
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-            Get.toNamed(listRoutes[index], id: 1);
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.grid_on),
-            title: Text("Tareas"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("Cursos"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            title: Text("Perfil"),
-          ),
-        ],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.circular(25),
+        child: BottomNavigationBar(
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.black,
+          type: BottomNavigationBarType.fixed,
+          showUnselectedLabels: true,
+          currentIndex: _currentIndex,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+              Get.toNamed(listRoutes[index], id: 1);
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.grid_on),
+              title: Text("Tareas"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Cursos"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              title: Text("Perfil"),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
 
 class VistaPrueba extends StatelessWidget {
   final name;
