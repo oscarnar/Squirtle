65bae6e5f1e89968a9861284ed5f34e41ea5083f
Sharon Rossely Chullunquía <schullunquia@unsa.edu.pe>
2020-08-17 14:06:08 -0500

    Identando lineas de codigo

--- lib/services/loginService.dart
+++ lib/services/loginService.dart
@@ -6,12 +6,12 @@ import 'package:squirtle/services/server.dart';
 
 Future<dynamic> loginService(
     {@required String username, @required String password}) async {
-      var url = Server.LoginURL;
-      var valueJSON;
-      var response = await http.post(url, body: {
-        'username': username,
-        'password': password,
-        'service': Server.LoginService,
+    var url = Server.LoginURL;
+    var valueJSON;
+    var response = await http.post(url, body: {
+      'username': username,
+      'password': password,
+      'service': Server.LoginService,
     });
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
